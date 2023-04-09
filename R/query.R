94e63247529802c1b33676f2bcff2bfa64f0d8c3
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-09 19:51:19 -0400

    Change pipe to R

--- R/query.R
+++ R/query.R
@@ -7,15 +7,15 @@
 #' @examples
 #' resp <- get_address("142.162.45.64")
 #'
-#' resp %>%
+#' resp |>
 #'   httr2::resp_body_string()
 get_address <- function(ip, format = "csv") {
 
-  httr2::request("http://ip-api.com") %>%
-    httr2::req_url_path_append(format) %>%
-    httr2::req_url_path_append(ip) %>%
-    httr2::req_user_agent("ipadress (https://github.com/clessn; info@clessn.ca)") %>%
-      httr2::req_throttle(45 / 60) %>%
+  httr2::request("http://ip-api.com") |>
+    httr2::req_url_path_append(format) |>
+    httr2::req_url_path_append(ip) |>
+    httr2::req_user_agent("ipadress (https://github.com/clessn; info@clessn.ca)") |>
+      httr2::req_throttle(45 / 60) |>
       httr2::req_perform()
 
 }
