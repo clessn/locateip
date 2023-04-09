e95d941ea136ba824830a6bafe32d6af6e8e7ffd
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-09 19:53:16 -0400

    Document `get_address()` (#3)
    * Remove internal function message
    
    * Remove ... parameter description
    
    * Describe IP parameter
    
    * Document format
    
    * Change title
    
    * Add example
    
    * Increment version number to 0.0.0.9001
    
    * Render documentation
    
    * Change pipe to R

--- R/query.R
+++ R/query.R
@@ -1,24 +1,21 @@
-#' Create request for IP API
+#' Locate an IP adress
 #'
-#' Internal function.
-#'
-#' @param ip IP
-#' @param ... Extra parameters to add to the query.
-#' @return A request.
+#' @param ip A single IPv4/IPv6 address or a domain name. If you don't supply a query the current IP address will be used.
+#' @param format Json, xml, csv, newline or php.
+#' @return A response.
 #' @export
 #' @examples
+#' resp <- get_address("142.162.45.64")
+#'
+#' resp |>
+#'   httr2::resp_body_string()
 get_address <- function(ip, format = "csv") {
 
-  httr2::request("http://ip-api.com") %>%
-    httr2::req_url_path_append(format) %>%
-    httr2::req_url_path_append(ip) %>%
-    httr2::req_user_agent("ipadress (https://github.com/clessn; info@clessn.ca)") %>%
-      httr2::req_throttle(45 / 60) %>%
+  httr2::request("http://ip-api.com") |>
+    httr2::req_url_path_append(format) |>
+    httr2::req_url_path_append(ip) |>
+    httr2::req_user_agent("ipadress (https://github.com/clessn; info@clessn.ca)") |>
+      httr2::req_throttle(45 / 60) |>
       httr2::req_perform()
 
 }
-
-test <- get_address("142.162.45.64")
-
-test %>%
-  httr2::resp_body_string()
