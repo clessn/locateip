73812a053d4ae79c23100e433e544f8c407c3d9a
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-10 01:17:51 -0400

    Create function to check ip adress parameter (#20)
    * Create validate_ipv4()
    
    * Add first version of ipv6
    
    * Fix typo
    
    * Create validate_ip()
    
    * Add testthat
    
    * Create tests for validate_ipv4 and ipv6()
    
    * Add test for validate_ip
    
    * Create documentation, edit return
    
    * Add error message
    
    * Increment version number to 0.0.0.9003

--- R/locate_ip.R
+++ R/locate_ip.R
@@ -12,12 +12,39 @@
 #' resp |>
 #'   httr2::resp_body_string()
 locate_ip <- function(ip, format = "csv") {
-
-  httr2::request("http://ip-api.com") |>
-    httr2::req_url_path_append(format) |>
-    httr2::req_url_path_append(ip) |>
-    httr2::req_user_agent("locateip (https://github.com/clessn; info@clessn.ca)") |>
+  if (validate_ip(ip) == FALSE) {
+    return(print("Pleade use a valid IP adress"))
+  } else {
+    resp <- httr2::request("http://ip-api.com") |>
+      httr2::req_url_path_append(format) |>
+      httr2::req_url_path_append(ip) |>
+      httr2::req_user_agent("locateip (https://github.com/clessn; info@clessn.ca)") |>
       httr2::req_throttle(45 / 60) |>
       httr2::req_perform()
 
+    return(resp)
+  }
+}
+
+#' Validate
+#' @param ip IP address in IPv4 or IPv6
+#' @return Logical.
+#' @describeIn validate Validate IP
+validate_ip <- function(ip) {
+  is_ipv4 <- validate_ipv4(ip)
+  is_ipv6 <- validate_ipv6(ip)
+
+  return(is_ipv4 | is_ipv6)
+}
+
+#' @describeIn validate Validate IPv4
+validate_ipv4 <- function(ip){
+  valid <- grepl("^((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)\\.?\\b){4}$", ip)
+  return(valid)
+}
+
+#' @describeIn validate Validate IPv6
+validate_ipv6 <- function(ip){
+  valid <- grepl("(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))", ip)
+  return(valid)
 }
---
+++ tests/testthat/test-locate_ip.R
@@ -0,0 +1,16 @@
+test_that("validate ipv6 works", {
+  expect_equal(validate_ipv6("2a01:cb08:832a:500:d9e3:a1d0:3d20:5f7e"),
+               TRUE)
+})
+
+test_that("validate ipv4 works", {
+  expect_equal(validate_ipv4("142.162.45.64"),
+               TRUE)
+})
+
+test_that("validate ip works", {
+  expect_equal(validate_ip("142.162.45.64"),
+               TRUE)
+  expect_equal(validate_ip("2a01:cb08:832a:500:d9e3:a1d0:3d20:5f7e"),
+               TRUE)
+})
