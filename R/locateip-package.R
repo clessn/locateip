48b44f488bf1dea399cac736619f060d63b8879e
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-09 20:19:48 -0400

    Add httr2 imports

--- R/locateip-package.R
+++ R/locateip-package.R
@@ -2,6 +2,10 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom httr2 req_perform
+#' @importFrom httr2 req_throttle
+#' @importFrom httr2 req_url_path_append
+#' @importFrom httr2 req_user_agent
 #' @importFrom httr2 request
 ## usethis namespace: end
 NULL
