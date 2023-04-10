d558cd04af4a7c1cf7d5695506999bb97db6a5f0
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-10 13:56:35 -0400

    Reorder functions

--- R/locate.R
+++ R/locate.R
@@ -18,6 +18,47 @@ locate_ip <- function(ip, fields = c("status,message,country,city,query"), ...) 
   return(string)
 }
 
+#' Get location
+#'
+#' `r lifecycle::badge('experimental')`
+#'
+#' Internal function.
+#'
+#' For API documentation and terms of service, see [ip-api.com](https://ip-api.com/).
+#'
+#' @param ip A single IPv4/IPv6 address or a domain name. If you don't supply a query the current IP address will be used.
+#' @param fields Response fields to pass on to the API.
+#' @param ... Query parameters to pass on to the API.
+#' @param format Json, xml, csv, newline or php.
+#' @return A response.
+#' @export
+#' @examples
+#' resp <- get_location("142.162.45.64")
+#'
+#' resp |>
+#'   httr2::resp_body_string()
+get_location <- function(ip, fields = c("status,message,country,city,query"), ..., format = "csv") {
+  if (validate_ip(ip) == FALSE) {
+    return(print("Pleade use a valid IP adress"))
+  } else {
+    params <- list(
+      fields = fields,
+      ...
+    )
+
+
+    resp <- httr2::request("http://ip-api.com") |>
+      httr2::req_url_path_append(format) |>
+      httr2::req_url_path_append(ip) |>
+      httr2::req_url_query(!!!params) |>
+      httr2::req_user_agent("locateip (https://github.com/clessn; info@clessn.ca)") |>
+      httr2::req_throttle(45 / 60) |>
+      httr2::req_perform()
+
+    return(resp)
+  }
+}
+
 #' Tidy location string into a tibble
 #'
 #' @description
@@ -59,45 +100,4 @@ tidy_location <- function(response = NULL, fields = NULL) {
     tibble::as_tibble()
 
   return(data)
-}
-
-#' Get location
-#'
-#' `r lifecycle::badge('experimental')`
-#'
-#' Internal function.
-#'
-#' For API documentation and terms of service, see [ip-api.com](https://ip-api.com/).
-#'
-#' @param ip A single IPv4/IPv6 address or a domain name. If you don't supply a query the current IP address will be used.
-#' @param fields Response fields to pass on to the API.
-#' @param ... Query parameters to pass on to the API.
-#' @param format Json, xml, csv, newline or php.
-#' @return A response.
-#' @export
-#' @examples
-#' resp <- get_location("142.162.45.64")
-#'
-#' resp |>
-#'   httr2::resp_body_string()
-get_location <- function(ip, fields = c("status,message,country,city,query"), ..., format = "csv") {
-  if (validate_ip(ip) == FALSE) {
-    return(print("Pleade use a valid IP adress"))
-  } else {
-    params <- list(
-      fields = fields,
-      ...
-    )
-
-
-    resp <- httr2::request("http://ip-api.com") |>
-      httr2::req_url_path_append(format) |>
-      httr2::req_url_path_append(ip) |>
-      httr2::req_url_query(!!!params) |>
-      httr2::req_user_agent("locateip (https://github.com/clessn; info@clessn.ca)") |>
-      httr2::req_throttle(45 / 60) |>
-      httr2::req_perform()
-
-    return(resp)
-  }
 }
