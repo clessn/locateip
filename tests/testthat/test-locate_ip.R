1b483cb81ce6feb5729c9262910489e64b1b866c
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-10 01:01:49 -0400

    Add test for validate_ip

--- tests/testthat/test-locate_ip.R
+++ tests/testthat/test-locate_ip.R
@@ -7,3 +7,10 @@ test_that("validate ipv4 works", {
   expect_equal(validate_ipv4("142.162.45.64"),
                TRUE)
 })
+
+test_that("validate ip works", {
+  expect_equal(validate_ip("142.162.45.64"),
+               NULL)
+  expect_equal(validate_ip("2a01:cb08:832a:500:d9e3:a1d0:3d20:5f7e"),
+               NULL)
+})
