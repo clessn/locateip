599d44bc1da58524b047dfbbd049fc3e30984f03
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-10 12:17:27 -0400

    Add passing IPv4

--- tests/testthat/test-validate.R
+++ tests/testthat/test-validate.R
@@ -4,12 +4,24 @@ test_that("validate ipv6 works", {
 })
 
 test_that("validate ipv4 works", {
+  # Valid
   expect_equal(validate_ipv4("142.162.45.64"),
                TRUE)
   expect_equal(validate_ipv4("0.0.0.0"),
                TRUE)
   expect_equal(validate_ipv4("127.255.255.255"),
                TRUE)
+  expect_equal(validate_ipv4("128.0.0.0"),
+               TRUE)
+  expect_equal(validate_ipv4("191.255.255.255"),
+               TRUE)
+  expect_equal(validate_ipv4("192.0.0.0"),
+               TRUE)
+  expect_equal(validate_ipv4("223.255.255.255"),
+               TRUE)
+
+  # Not valid
+
 })
 
 test_that("validate ip works", {
