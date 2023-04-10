2165bfaa6adc26dd75b02e644fd62d1dd2321c0d
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-10 12:13:39 -0400

    Move validate functions to validate.R

---
+++ R/validate.R
@@ -0,0 +1,36 @@
+#' Validate an IP address
+#'
+#' @description
+#' `r lifecycle::badge("experimental")`
+#'
+#' Internal function.
+#'
+#' Validate an IP adress as IPv4 or IPv6.
+#'
+#' @param ip String. IP address as IPv4 or IPv6.
+#' @return Logical. TRUE if it's either IPv4 or IPv6, FALSE if not.
+#'
+#' @examples
+#'
+#' validate_ip("0.0.0.0")
+#'
+#' @describeIn validate Validate IPv4 or IPv6
+#' @export
+validate_ip <- function(ip) {
+  is_ipv4 <- validate_ipv4(ip)
+  is_ipv6 <- validate_ipv6(ip)
+
+  return(is_ipv4 | is_ipv6)
+}
+
+#' @describeIn validate Validate IPv4
+validate_ipv4 <- function(ip){
+  valid <- grepl("^((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)\\.?\\b){4}$", ip)
+  return(valid)
+}
+
+#' @describeIn validate Validate IPv6
+validate_ipv6 <- function(ip){
+  valid <- grepl("(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))", ip)
+  return(valid)
+}
