72d1b9a2cceeee3280d19fbd4b3e6cdf66d0cd02
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-10 12:43:19 -0400

    Create `get_location()`, extract response in `locate_ip()` (#39)
    * Create get_location()
    
    * Replace code inside locate_ip()
    
    * Remove format parameter from locate_ip()
    
    * Return string with locate_ip()
    
    * Update example in README

--- R/locate.R
+++ R/locate.R
@@ -5,15 +5,37 @@
 #' For API documentation and terms of service, see [ip-api.com](https://ip-api.com/).
 #'
 #' @param ip A single IPv4/IPv6 address or a domain name. If you don't supply a query the current IP address will be used.
+#' @return A string.
+#' @export
+#' @examples
+#' locate_ip("142.162.45.64")
+locate_ip <- function(ip) {
+  resp <- get_location(ip, format = "csv")
+
+  string <- resp |>
+    httr2::resp_body_string()
+
+  return(string)
+}
+
+#' Get location
+#'
+#' `r lifecycle::badge('experimental')`
+#'
+#' Internal function.
+#'
+#' For API documentation and terms of service, see [ip-api.com](https://ip-api.com/).
+#'
+#' @param ip A single IPv4/IPv6 address or a domain name. If you don't supply a query the current IP address will be used.
 #' @param format Json, xml, csv, newline or php.
 #' @return A response.
 #' @export
 #' @examples
-#' resp <- locate_ip("142.162.45.64")
+#' resp <- get_location("142.162.45.64")
 #'
 #' resp |>
 #'   httr2::resp_body_string()
-locate_ip <- function(ip, format = "csv") {
+get_location <- function(ip, format = "csv") {
   if (validate_ip(ip) == FALSE) {
     return(print("Pleade use a valid IP adress"))
   } else {
