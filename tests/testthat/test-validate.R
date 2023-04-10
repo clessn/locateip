0928f4be6abfeb9185c4c5322ac9a621e77c01a4
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-10 12:21:46 -0400

    Add ipv6

--- tests/testthat/test-validate.R
+++ tests/testthat/test-validate.R
@@ -1,6 +1,11 @@
 test_that("validate ipv6 works", {
   expect_equal(validate_ipv6("2a01:cb08:832a:500:d9e3:a1d0:3d20:5f7e"),
                TRUE)
+
+  expect_equal(validate_ipv6("2001:0db8:0000:0000:0000:ff00:0042:8329"),
+               TRUE)
+  expect_equal(validate_ipv6("2001:db8::ff00:42:8329"),
+               TRUE) # Abbreviation of previous
 })
 
 test_that("validate ipv4 works", {
